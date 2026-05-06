ad97d6fecd512f7b564ac51eb315538b543b94ed
mukeshbhatt18gl <mukesh.bhatt@qlik.com>
2026-05-06 12:33:52 +0530

    reslove additional properties issue

--- tap_servicenow/concurrent_discovery.py
+++ tap_servicenow/concurrent_discovery.py
@@ -228,7 +228,7 @@ class ServiceNowTableSchemaBuilder(ConcurrentDiscovery):
                     table,
                 )
 
-            schema = {"type": "object", "properties": properties}
+            schema = {"type": "object", "properties": properties, "additionalProperties": False}
 
             # Lightweight access probe (1 record, no count)
             try:
