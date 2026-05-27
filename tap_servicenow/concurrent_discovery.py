4917e9fbd86c419e16fcf94a66cdc47ad6b56d6b
Mukesh Bhatt <mukesh.bhatt@qlik.com>
2026-05-27 13:59:56 +0530

    Update tap_servicenow/concurrent_discovery.py
    Co-authored-by: Rushikesh Todkar <98420315+RushiT0122@users.noreply.github.com>

--- tap_servicenow/concurrent_discovery.py
+++ tap_servicenow/concurrent_discovery.py
@@ -228,7 +228,11 @@ class ServiceNowTableSchemaBuilder(ConcurrentDiscovery):
                     table,
                 )
 
-            schema = {"type": "object", "properties": properties, "additionalProperties": False}
+            schema = {
+                "type": "object",
+                "properties": properties,
+                "additionalProperties": False
+            }
 
             # Lightweight access probe (1 record, no count)
             try:
