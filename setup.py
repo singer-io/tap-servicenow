9a530a949c7df73f5d594280cdad69e59f74da43
Rutuja Deshmukh <107538720+rdeshmukh15@users.noreply.github.com>
2026-05-28 10:45:26 +0530

    Bump version from 0.0.2 to 0.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="tap-servicenow",
-      version="0.0.2",
+      version="0.0.1",
       description="Singer.io tap for extracting data from ServiceNow API",
       author="Stitch",
       url="http://singer.io",
